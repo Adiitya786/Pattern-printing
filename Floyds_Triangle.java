013eee010c98b50b933209d2e16a02c3ac4e3bb3
Adiitya786 <adityaprajapatii786@gmail.com>
2025-07-26 22:48:38 +0530

    Program to print Floyds Triangle.

---
+++ Floyds_Triangle.java
@@ -0,0 +1,14 @@
+public class Floyds_Triangle {
+    public static void main(String[] args) {
+        int rows =5;
+        int a=1;
+         for(int i =1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(a);
+                a++;
+            }
+            System.out.println("");
+            
+        }
+    }
+}
