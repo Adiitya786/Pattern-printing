43f3015104d913528acf96b5dcf0beb883384755
Adiitya786 <adityaprajapatii786@gmail.com>
2025-07-26 22:43:47 +0530

    Program of inverted  half pyramid with numbers.

---
+++ half_pyramid_number.java
@@ -0,0 +1,15 @@
+public class half_pyramid_number {
+    public static void main(String[] args) {
+        int rows =5;
+         for(int i =1;i<=rows;i++){
+            // spaces
+            for(int j=1;j<=rows-i+1;j++){
+                System.out.print(j);
+                
+            }
+            System.out.println("");
+            
+        }
+    }
+    
+}
